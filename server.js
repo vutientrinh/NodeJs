519a12cbce93e1d12c12be3d5e7ba0a57bc678c7
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2024-10-13 00:48:45 +0700

    Config data

--- server.js
+++ server.js
@@ -1,32 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.get('/', (req, res) => {
-    res.json({
-        "message": "Hello"
-    });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello",
+  });
 });
-require('./app/routers/note.routers.js')(app);
+require("./app/routers/note.routers.js")(app);
 app.listen(3000, () => {
-    console.log("Listening in port 3000");
+  console.log("Listening in port 3000");
 });
 
 // Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
+const dbConfig = require("./config/database.config.js");
+const dbConfig = process.env.MONGO_URL || "mongodb://localhost:27017/node";
+
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
     console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
     process.exit();
-});
+  });
