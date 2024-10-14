1a9bcf2d1bcf7c1cc59cc383ea414e78e8986c1a
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2024-10-14 07:46:20 +0700

    Update

--- config/database.config.js
+++ config/database.config.js
@@ -1,9 +1,8 @@
-// module.exports = {
-//   url: "mongodb://localhost:27017/node",
-//     //   url: process.env.MONGO_URL
-
-// };
-
 module.exports = {
-  url: `${process.env.MONGO_URL}/${process.env.DB_NAME}`,
+  url: "mongodb://localhost:27017/node",
+  //   url: process.env.MONGO_URL
 };
+
+// module.exports = {
+//   url: `${process.env.MONGO_URL}/${process.env.DB_NAME}`,
+// };
